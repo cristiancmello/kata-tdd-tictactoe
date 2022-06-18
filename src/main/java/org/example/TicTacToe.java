6ec3248e934921221bf49e239dc880ce0b2c9c95
Cristian Mello <cristianc.mello@gmail.com>
2022-06-18 18:48:11 -0300

    Adding beforeEach into TicTacToeTest

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class TicTacToe {
+  public void play(int x, int y) {
+    if (x < 1 || x > 3) {
+      throw new RuntimeException("X esta fora do tabuleiro");
+    }
+  }
 }
