562d41ff7d558f9a01e31f7c11f7f323a517816a
Cristian Mello <cristianc.mello@gmail.com>
2022-06-18 19:13:35 -0300

    Refact TicTacToe play

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -4,6 +4,8 @@ public class TicTacToe {
   public void play(int x, int y) {
     if (x < 1 || x > 3) {
       throw new RuntimeException("X esta fora do tabuleiro");
+    } else if (y < 1 || y > 3) {
+      throw new RuntimeException("Y esta fora do tabuleiro");
     }
   }
 }
