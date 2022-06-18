ec7b30267d0a553e3821e6fb87b94c1d0fd40e18
Cristian Mello <cristianc.mello@gmail.com>
2022-06-18 19:31:06 -0300

    Impl quandoLocalOcupadoEntaoRuntimeException to pass

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,11 +1,23 @@
 package org.example;
 
 public class TicTacToe {
+  private Character[][] board = {
+    {'\0', '\0', '\0'},
+    {'\0', '\0', '\0'},
+    {'\0', '\0', '\0'}
+  };
+
   public void play(int x, int y) {
     if (x < 1 || x > 3) {
       throw new RuntimeException("X esta fora do tabuleiro");
     } else if (y < 1 || y > 3) {
       throw new RuntimeException("Y esta fora do tabuleiro");
+    }
+
+    if (board[x - 1][y - 1] != '\0') {
+      throw new RuntimeException("Espaco ocupado");
+    } else {
+      board[x - 1][y - 1] = 'X';
     }
   }
 }
