3259a478a6f2eea0dd3278fc660c9905c9f77cc0
Cristian Mello <cristianc.mello@gmail.com>
2022-06-18 23:01:43 -0300

    Adding quandoJogarETodaDiagonalBaixoCimaEntaoVitoria test

--- src/test/java/org/example/TicTacToeTest.java
+++ src/test/java/org/example/TicTacToeTest.java
@@ -96,4 +96,15 @@ public class TicTacToeTest {
     String actual = ticTacToe.play(3, 3);
     assertEquals("X é o vencedor", actual);
   }
+
+  @Test
+  public void quandoJogarETodaDiagonalBaixoCimaEntaoVitoria() {
+    ticTacToe.play(1, 3);
+    ticTacToe.play(1, 1);
+    ticTacToe.play(2, 2);
+    ticTacToe.play(1, 2);
+
+    String actual = ticTacToe.play(3, 1);
+    assertEquals("X é o vencedor", actual);
+  }
 }
