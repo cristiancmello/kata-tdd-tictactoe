1648af9e0aedcbb9576bb33935eda56064bdcb38
Cristian Mello <cristianc.mello@gmail.com>
2022-06-18 21:26:45 -0300

    Return old code..

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -9,13 +9,11 @@ public class TicTacToe {
 
   private char lastPlayer = '\0';
 
-  public String play(int x, int y) {
+  public void play(int x, int y) {
     checkAxis(x);
     checkAxis(y);
     setBox(x, y);
     lastPlayer = nextPlayer();
-
-    return "Sem vencedor";
   }
 
   private void checkAxis(int axis) {
